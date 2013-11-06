07d72702330eb52be2ffa9b81129edd5d5a7fef7
Mike Wleklinski <michael@hardindd.com>
2013-11-06 12:57:36 -0600

    will run but not able to load thumbnails

--- src/com/test/testappwithyoutube/MainActivity.java
+++ src/com/test/testappwithyoutube/MainActivity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -13,6 +14,7 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubeThumbnailLoader;
 import com.google.android.youtube.player.YouTubeThumbnailLoader.ErrorReason;
 import com.google.android.youtube.player.YouTubeThumbnailView;
+import com.test.testappwithyoutube.adapter.VideoListAdapter;
 
 public class MainActivity extends Activity {
 	
@@ -21,15 +23,18 @@ public class MainActivity extends Activity {
 	TextView videoTitle;
 	YouTubeThumbnailView thumbView;
 	ArrayList<YouTubeThumbnailView> thumbViews = new ArrayList<YouTubeThumbnailView>();
+	VideoListAdapter videoAdapter;
 	YouTubeThumbnailLoader thumbnailLoader;
+	Activity mActivity;
 	
-	public final String DEV_KEY = "AIzaSyCCKxzeVDL0GfJGfVnE9J-xVMkvBrO4c-s";
-	public final String PLAYLIST_ID = "PLAC90ED58CDC8A748";
+	public final String DEV_KEY = "";
+	public final String PLAYLIST_ID = "ECAE6B03CA849AD332";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mActivity = this;
 		
 		videoList = (ListView) findViewById(R.id.video_list);
 		videoThumb = (ImageView) findViewById(R.id.video_thumb);
@@ -41,6 +46,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onInitializationFailure(YouTubeThumbnailView thumbView, YouTubeInitializationResult result) {
 				// TODO Auto-generated method stub
+				Log.i("initFail", result.toString());
 				
 			}
 
@@ -55,18 +61,20 @@ public class MainActivity extends Activity {
 						if(thumbnailLoader.hasNext())
 							thumbnailLoader.next();
 						else {
-							
+							videoAdapter = new VideoListAdapter(mActivity, thumbViews);
+							videoList.setAdapter(videoAdapter);
 						}
 						
 					}
 					
 					@Override
 					public void onThumbnailError(YouTubeThumbnailView arg0, ErrorReason arg1) {
-						// TODO Auto-generated method stub
+						Log.i("initFail", arg1.toString());
 						
 					}
 				});
 			    thumbnailLoader.setPlaylist(PLAYLIST_ID);
+			    thumbnailLoader.first();
 				
 			}
 			
--- src/com/test/testappwithyoutube/adapter/VideoListAdapter.java
+++ src/com/test/testappwithyoutube/adapter/VideoListAdapter.java
@@ -2,16 +2,36 @@ package com.test.testappwithyoutube.adapter;
 
 import java.util.ArrayList;
 
-import com.test.testappwithyoutube.R;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import com.google.android.youtube.player.YouTubeThumbnailView;
+import com.test.testappwithyoutube.R;
 
 public class VideoListAdapter extends ArrayAdapter<YouTubeThumbnailView> {
+	
+	private final Context context;
+	private final ArrayList<YouTubeThumbnailView> values;
 
 	public VideoListAdapter(Context context, ArrayList<YouTubeThumbnailView> objects) {
 		super(context, R.layout.list_item_video, objects);
+		this.context = context;
+		this.values = objects;
 	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View rowView = inflater.inflate(R.layout.list_item_video, parent, false);
 
+		ImageView vidThumb = (ImageView) rowView.findViewById(R.id.video_thumb);
+		vidThumb.setImageDrawable(values.get(position).getDrawable());
+		
+		return rowView;
+		
+	}
 }
